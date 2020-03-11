b7285691135d7f515502fd57c98e40acae58d8da
Kristoffer Bråthen Halden <>
2020-03-11 13:37:07 +0100

    Done and dusted

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // styles
 import './css/index.css';
-
-
+import Welcome from './components/Welcome.js';
+import Image from './components/Image.js';
+import Panel from './components/Panel.js';
+import NestedComponentExample from './components/NestedComponentExample.js';
 ReactDOM.render(<App />, document.getElementById('root'));
-
 
 function App() {
     return (
@@ -21,34 +22,5 @@ function App() {
   
             <NestedComponentExample />
         </div>
-    );
-}
-  
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
-}
-  
-function Image(props) {
-    return (
-        <img src={props.source} alt={props.altText} />
-    );
-}
-  
-function Panel(props) {
-    return (
-        <div className="panel">
-            <h2 className="panel__heading">{props.heading}</h2>
-            <div className="panel__content">
-                {props.children}
-            </div>
-        </div>
-    );
-}
-  
-function NestedComponentExample(props) {
-    return (
-        <Panel heading="Component with nested components">
-            <Image source="https://placedog.net/500/280/random" alt="Random dog" />
-        </Panel>
     );
 }
